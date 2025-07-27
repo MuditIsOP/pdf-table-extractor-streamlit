ef7fe309fdeac9b848e799f65579ec7700c070bc
Mudit Sharma <94481502+MuditIsOP@users.noreply.github.com>
2025-07-27 20:45:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ if st.button("Start") and uploaded_files:  # ← Add button logic for control
 
                         # Clean dashes and newlines in specific columns
                         if df.shape[1] > 8:  # ← safeguard
-                            for col in [df.columns[6], df.columns[8]]:
+                            for col in [df.columns[9], df.columns[8]]:
                                         df[col] = (df[col].astype(str).str.replace(r'\n+', ' ', regex=True).str.replace(r'[—–\-]+', '\n', regex=True).str.replace('ENGINEERIN G','ENGINEERING').str.strip())
                     dfs.append(df)
 
